93fad3d755d81aa4c61c7eade8fe4f28dbf19f90
ASAD2723 <khanasad323@gmail.com>
2023-06-17 18:00:48 +0530

    DoneCRUDOperations

--- app/api/blogs/[id]/route.ts
+++ app/api/blogs/[id]/route.ts
@@ -1,11 +1,44 @@
+import { deletePost, getById, updatePost } from "@/lib/data";
+import { NextResponse } from "next/server";
+
 export const GET =async (req:Request) => {
-    console.log("GET")
+    try {
+        const id = req.url.split("blogs/")[1];
+        const post = getById(id);
+        if(!post) {
+            return NextResponse.json({ message: "ERROR" }, { status: 404 })
+        }
+        return NextResponse.json({ message: "OK", post }, { status: 200 })
+     }  catch (err) {
+        return NextResponse.json({ message: "ERROR", err }, { status: 500 })
+    }
+    
+    
 }
+
+
+export const PUT =async (req:Request) => {
+    try {
+        const { title, desc } = await req.json();
+        const id = req.url.split("blogs/")[1];
+        updatePost(id, title, desc);
+        return NextResponse.json({message:"OK"}, {status:200})
+    } catch (err) {
+        return NextResponse.json({message:"ERROR", err}, {status:500})
+    }
+};
+
 
 export const POST =async (req:Response) => {
     console.log("POST")
 }
 
 export const DELETE = async (req:Request) => {
-    console.log("DELETE")
+    try {
+        const id = req.url.split("blogs/")[1];
+        deletePost(id);
+        return NextResponse.json({message:"OK"}, {status:200})
+    } catch (err) {
+        return NextResponse.json({message:"ERROR", err}, {status:500})
+    }
 }
