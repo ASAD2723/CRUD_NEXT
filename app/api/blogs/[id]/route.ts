7f8f694f62e3a7d58d2eef2a603eac7fdd1271c1
ASAD2723 <khanasad323@gmail.com>
2023-06-17 17:17:07 +0530

    getpostCRUD

---
+++ app/api/blogs/[id]/route.ts
@@ -0,0 +1,11 @@
+export const GET =async (req:Request) => {
+    console.log("GET")
+}
+
+export const POST =async (req:Response) => {
+    console.log("POST")
+}
+
+export const DELETE = async (req:Request) => {
+    console.log("DELETE")
+}
--- app/api/blogs/route.ts
+++ app/api/blogs/route.ts
@@ -1,5 +1,15 @@
+import { getPosts } from "@/lib/data";
+import { NextResponse } from "next/server"
+
 export const GET =async (req: Request, res: Response) => {
-    console.log("GET REQUEST")
+    try {
+        const posts = getPosts();
+        return NextResponse.json({message: "OK", posts }, {status:200})
+    } catch (err) {
+        return NextResponse.json({message:"Error", err}.err,{
+            status: 500,
+        });
+    }
 }
 
 export const POST =async (req: Request, res: Response) => {
